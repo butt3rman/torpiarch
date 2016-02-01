e4fede7aeaef98a61555da6c8e536e2b002fa206
butt3rman <butt3rman@tuta.io>
2016-02-01 08:10:18 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ echo -n "Testing Internet connection ... "
 ping -q -w 1 -c 1 google.fr > /dev/null && echo "OK" || (echo "Your Internet seems broken. Press Ctrl-C to abort or enter to continue." && read)
 # rankmirrors to make this faster (though it takes a while)
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -o /etc/pacman.d/mirrorlist.new https://www.archlinux.org/mirrorlist/?country=all&protocol=http
+curl -o /etc/pacman.d/mirrorlist.new https://raw.githubusercontent.com/archlinuxarm/PKGBUILDs/master/core/pacman-mirrorlist/mirrorlist
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist.new
 echo -en "\nRanking the mirrors. Please wait... (This may take a while)"
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.new > /etc/pacman.d/mirrorlist
